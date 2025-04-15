76d20239e9ac0caeb2953258dd8c731f7747b9ae
Allan <aavendan@gmail.com>
2025-04-15 15:28:46 -0500

    styling

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,11 +27,18 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
-html_static_path = ['_static']
-
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
