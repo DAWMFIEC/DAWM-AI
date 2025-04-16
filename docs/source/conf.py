4d767a25c345f5b009192a0f3190bd10f2694cfd
Allan <aavendan@gmail.com>
2025-04-16 13:39:20 -0500

    owning

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'DAWM'
+copyright = '2025, ARAS'
+author = 'Allan Avendaño'
 
 release = '0.1'
 version = '0.1.0'
